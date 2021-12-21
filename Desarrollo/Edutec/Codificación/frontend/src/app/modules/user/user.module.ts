5dd6bbc4027bfaee7214d171594aa523d8987405
Angel Romaní <angrodi@outlook.com>
2021-12-20 22:51:42 -0500

    Actualización de Burndown Chart - Sprint 2

--- Desarrollo/Edutec/Codificación/frontend/src/app/modules/user/user.module.ts
+++ Desarrollo/Edutec/Codificación/frontend/src/app/modules/user/user.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { UserRoutingModule } from './user-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
